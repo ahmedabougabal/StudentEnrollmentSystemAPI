f1b3dbf5e0edc100757608b2191acdc9fe5f3303
ahmedabougabal <ahmedabougabal@live.com>
2025-03-09 12:51:36 +0200

    feat: enhance PaginatedResult DTO

--- Application/DTOs/PaginatedResult.cs
+++ Application/DTOs/PaginatedResult.cs
@@ -5,6 +5,8 @@ public class PaginatedResult<T>
     public IEnumerable<T> Items { get; set; } = new List<T>();
     public int TotalCount { get; set; }
     public int PageNumber { get; set; }
+    public int Page { get; set; }
+
     public int PageSize { get; set; }
     public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
     public bool HasPreviousPage => PageNumber > 1;
