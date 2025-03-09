f84705de15a788a399c56be53d8570d3c057160c
ahmedabougabal <ahmedabougabal@live.com>
2025-03-09 13:52:20 +0200

    feat: enhance PaginatedResult with factory method and rename Page to PageNumber

--- Application/DTOs/PaginatedResult.cs
+++ Application/DTOs/PaginatedResult.cs
@@ -5,10 +5,19 @@ public class PaginatedResult<T>
     public IEnumerable<T> Items { get; set; } = new List<T>();
     public int TotalCount { get; set; }
     public int PageNumber { get; set; }
-    public int Page { get; set; }
-
     public int PageSize { get; set; }
     public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
     public bool HasPreviousPage => PageNumber > 1;
     public bool HasNextPage => PageNumber < TotalPages;
+
+    public static PaginatedResult<T> Create(IEnumerable<T> items, int totalCount, int pageNumber, int pageSize)
+    {
+        return new PaginatedResult<T>
+        {
+            Items = items,
+            TotalCount = totalCount,
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+    }
 }
